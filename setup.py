691c9ffa7b219b03ca0cf519e9992b014e15656b
Mattias Lagergren <mattias.lagergren@ftrack.com>
2015-02-24 13:28:19 +0000

    Remove inclusion of assetmgr and FnAssetAPI

--- setup.py
+++ setup.py
@@ -273,9 +273,7 @@ if sys.platform in ('darwin', 'win32', 'linux2'):
             'ftrack_connect_nuke_studio.plugin',
             'atexit',  # Required for PySide
             'ftrack_connect_cinesync.cinesync_launcher',
-            'ftrack_connect.application',
-            'assetmgr_hiero',
-            'FnAssetAPI'
+            'ftrack_connect.application'
         ],
         'excludes': [
             # The following don't actually exist, but are picked up by the
